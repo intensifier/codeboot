760026504b16fbca0cc445e3ecb84614246a0eb7
Marc-André Bélanger <marc-andre.belanger@asciimail.com>
2022-01-21 17:15:59 -0500

    Turbocharge drawLine.

--- include/charts.js
+++ include/charts.js
@@ -13,26 +13,43 @@ const codebootCharts = (function () {
   // register vega and vega-lite with the API
   vl.register(vega, vegaLite, vegaLiteOptions);
 
-  function drawLine(csv, xLabel, yLabel, xType) {
+  function drawLine(csv, xDesc, yDesc) {
     var chart = document.getElementsByClassName('cb-html-window')[0];
 
     var data = csv_to_vega(csv);
 
-    // Clear the last chart
-    chart.innerHTML = "";
+    var xField, xTooltip, xLabel, xType;
+    var yField, yTooltip, yLabel, yType;
 
-    if (xType === 'timestamp') {
+    [xLabel, xType] = xDesc.split(':');
+    [yLabel, yType] = yDesc.split(':');
+
+    if (xType === "unix_timestamp") {
+      // Scale to 1000x
+      data.forEach(e => {
+        e[xLabel] = 1000 * e[xLabel];
+      })
+
       xField = vl.x().fieldT(xLabel);
+      xTooltip = vl.fieldT(xLabel);
     } else {
       xField = vl.x().fieldQ(xLabel);
+      xTooltip = vl.fieldQ(xLabel);
     }
+
+    // TODO: Handle yType
+    yField = vl.y().fieldQ(yLabel);
+    yTooltip = vl.fieldQ(yLabel);
+
+    // Clear the last chart
+    chart.innerHTML = "";
 
     vl.markLine({ tooltip: true })
       .data(data)
       .encode(
         xField,
-        vl.y().fieldQ(yLabel),
-        vl.tooltip([vl.fieldQ(xLabel), vl.fieldQ(yLabel)])
+        yField,
+        vl.tooltip([xTooltip, yTooltip])
       )
       .render()
       .then(viewElement => {
